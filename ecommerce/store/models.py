dc82a567bdd0f82f3f961b5663b742d942c18bbe
Deaath65 <deaath65@gmail.com>
2023-08-02 10:43:41 +0545

    ShippingAddress Model

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -40,3 +40,14 @@ class OrderItem(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
     
     
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.address
