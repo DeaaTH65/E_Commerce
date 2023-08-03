1cfdc4d165e7450b2ec4357fba1ef3ec38b51924
Deaath65 <deaath65@gmail.com>
2023-08-03 21:14:07 +0545

    add product javascript

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -9,7 +9,30 @@ for(var i=0; i < updateBtns.length; i++){
         if(user === 'AnonymousUser'){
             console.log('Not logger in')
         }else{
-            console.log('Sending data...')
+            updateUserOrder(productId, action)
         }
+    })
+}
+
+
+function updateUserOrder(productId, action){
+    console.log('Sending data...')
+    var url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken':csrftoken,
+        },
+        body:JSON.stringify({'productId': productId, 'action': action})
+    })
+
+    .then((response) =>{
+        return response.json
+    })
+
+    .then((data) =>{
+        console.log('data:', data)
     })
 }
